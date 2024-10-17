9080ee021e1a51e01185d55354d2cf5439f55a44
Sat9995 <saidboy995@gmail.com>
2024-10-17 09:06:45 -0700

    Add files via upload

---
+++ Peremenye.py
@@ -0,0 +1,8 @@
+first_name = 'Python'
+total_tasks = 12
+hours_spent = 1.5
+print(1.5/12)
+hours_count = 0.125
+print('Курс:',first_name,','' всего задач:',
+      total_tasks , ',','затрачено часов:',
+      hours_spent, 'среднее выполнение:', hours_count, '.')
